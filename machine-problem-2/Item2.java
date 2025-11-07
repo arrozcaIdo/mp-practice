ffe151778da10c895c9c485db0704db4678b7ee2
Elyssa Marie Manango <mariemanango07@gmail.com>
2025-11-07 10:50:45 +0800

    FINAL

--- machine-problem-2/Item2.java
+++ machine-problem-2/Item2.java
@@ -1,17 +1,18 @@
 import java.util.*;
+//harshad-number
 
-public class Item2 {
+public class Item2{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a number: ");
-        int n = sc.nextInt(), temp = n, sum = 0;
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt(), sum = 0, temp = n;
 
         while(temp > 0){
-            sum += temp % 10;
+            sum += temp%10;
             temp /= 10;
         }
-
-        System.out.println(n % sum == 0 ? "Harshad Number" : "NOT a Harshad Number");
-
+        System.out.println(n % sum == 0 ? "Harshad!" : "NOT Harshad.");
     }
+
 }
