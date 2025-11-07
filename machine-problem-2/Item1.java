5f7a1e4f4459f7e28d78e0298ca3d435615b5b71
Elyssa Marie Manango <mariemanango07@gmail.com>
2025-11-07 09:01:38 +0800

    changed variable names, redid the logic

--- machine-problem-2/Item1.java
+++ machine-problem-2/Item1.java
@@ -1,14 +1,14 @@
 import java.util.*;
 
 public class Item1{
-    public static void main(String[] args){
+    public static void main (String[] args){
         System.out.println("Enter a number: ");
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt(), sum = 0;
+        int intNumber = sc.nextInt(), intSum = 0;
 
-        for(int i = 1; i <= n/2; i++)
-            if(n % i == 0) sum += i;
+        for(int i = 1; i <= intNumber/2; i++)
+            if(intNumber % i == 0) intSum += i;
 
-        System.out.println(sum == n ? "PERFECT NUMBER!" : "not a perfect number." );
+        System.out.println(intSum == intNumber ? "This is a PERFECT NUMBER!" : "This is NOT a perfect number.");
     }
 }
