d8617edfd9534b1727448a8b2c491f47e6d63209
Elyssa Marie Manango <mariemanango07@gmail.com>
2025-11-07 10:19:07 +0800

    FINAL

--- machine-problem-2/Item1.java
+++ machine-problem-2/Item1.java
@@ -1,14 +1,17 @@
 import java.util.*;
+//perfect-number-checker
 
 public class Item1{
-    public static void main (String[] args){
-        System.out.println("Enter a number: ");
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
         int intNumber = sc.nextInt(), intSum = 0;
 
         for(int i = 1; i <= intNumber/2; i++)
             if(intNumber % i == 0) intSum += i;
 
-        System.out.println(intSum == intNumber ? "This is a PERFECT NUMBER!" : "This is NOT a perfect number.");
+        System.out.println(intNumber == intSum ? "This is a PERFECT number!" : "This is NOT a perfect number.");
     }
+
 }
