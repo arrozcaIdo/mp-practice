29e3afb23ed15cd9be44e80ded4e7d6cde55f8cc
Elyssa Marie Manango <mariemanango07@gmail.com>
2025-11-08 14:59:29 +0800

    Harshad Number

--- machine-problem-2/Item2.java
+++ machine-problem-2/Item2.java
@@ -5,18 +5,17 @@ For example, 18 is a Harshad Number because 18 ÷ (1+8) = 2
 */
 
 public class Item2{
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter a number: ");
         int n = sc.nextInt(), sum = 0, temp = n;
 
-        while(temp > 0){
-            sum += temp%10;
+        while (temp > 0) {
+            sum += temp % 10;
             temp /= 10;
         }
 
-        System.out.println(n % sum == 0 ? "Harshad!" : "NOT Harshad.");
+        System.out.println(n % sum == 0 ? "HARSHAD!" : "not harshad.");
     }
-
 }
