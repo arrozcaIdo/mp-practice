3208789764a45bd7c0428e5d6965ef46e126f129
Elyssa Marie Manango <mariemanango07@gmail.com>
2025-11-20 03:44:26 +0800

    added menu

--- machine-problem-3/mp3.java
+++ machine-problem-3/mp3.java
@@ -3,7 +3,28 @@ import java.util.*;
 public class mp3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("This is a test");
+        int choice;
 
+        do {
+            System.out.println("---- String Manipulation Toolkit ----");
+            System.out.println("1. Reverse");
+            System.out.println("2. Palindrome");
+            System.out.println("3. Anagram");
+            System.out.println("4. Word Count");
+            System.out.println("5. Character Count");
+            System.out.println("6. Substring Finder");
+            System.out.println("7. Lowercase");
+            System.out.println("8. Uppercase");
+            System.out.println("9. Remove Vowels");
+            System.out.println("10. Remove Consonants");
+            System.out.println("11. Exit");
+
+            System.out.print("Choice: ");
+            choice = sc.nextInt();
+            sc.nextLine();
+
+
+
+        } while (choice != 11);
     }
 }
