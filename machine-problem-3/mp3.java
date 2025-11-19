7d5ab21014f1c5d0adfb4111089f40062c451f52
Elyssa Marie Manango <mariemanango07@gmail.com>
2025-11-20 03:18:13 +0800

    test #1

--- machine-problem-3/mp3.java
+++ machine-problem-3/mp3.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class mp3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("This is a test.");
+        System.out.println("This is a test");
 
     }
 }
