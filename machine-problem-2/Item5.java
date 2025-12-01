85cde353deadfbaae04797edab5d109ba19592cc
Elyssa Marie Manango <mariemanango07@gmail.com>
2025-12-01 19:39:57 +0800

    fix

--- machine-problem-2/Item5.java
+++ machine-problem-2/Item5.java
@@ -8,11 +8,19 @@ public class Item5 {
 
         long sq = (long) n * n;
         String s = Long.toString(sq);
+
+        boolean isKaprekar = false;
+
         for (int i = 1; i < s.length(); i++) {
             long left = Long.parseLong(s.substring(0, i));
             long right = Long.parseLong(s.substring(i));
 
-            System.out.println(left + right == n ? "Kaprekar Number!" : "NOT a Kaprekar number.");
+            if (left + right == n) {
+                isKaprekar = true;
+                break;
+            }
         }
+
+        System.out.println(isKaprekar ? "Kaprekar Number!" : "NOT a Kaprekar number.");
     }
 }
