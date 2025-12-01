7b241f6b3a0fc92c9cfaea9bbc404130570faf8a
Elyssa Marie Manango <mariemanango07@gmail.com>
2025-12-01 19:33:10 +0800

    initial

--- machine-problem-2/Item5.java
+++ machine-problem-2/Item5.java
@@ -1,8 +1,24 @@
+import java.util.*;
+
 public class Item5 {
-    public static void main(String[] args){
-        System.out.println("This is a test.");
-        for(int i = 0; i < 10; i++){
-            System.out.println(i);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        long sq = (long) n * n;
+        String s = Long.toString(sq);
+
+        for (int i = 1; i < s.length(); i++) {
+            long left = Long.parseLong(s.substring(0, i));
+            long right = Long.parseLong(s.substring(i));
+
+            if (right != 0 && left + right == n) {
+                System.out.println("Kaprekar Number");
+                return;
+            }
         }
+
+        System.out.println("Not a Kaprekar Number");
     }
 }
