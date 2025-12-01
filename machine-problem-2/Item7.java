c39f411181391c43eb909b6e8ec5a55a4cabd997
Elyssa Marie Manango <mariemanango07@gmail.com>
2025-12-01 20:35:59 +0800

    Twin Primes of a Number

--- machine-problem-2/Item7.java
+++ machine-problem-2/Item7.java
@@ -1,8 +1,27 @@
+import java.util.*;
+
+/* twin-primes-of-a-number
+Twin primes are two primes that differ by 2
+(e.g., (11, 13), (17, 19)).
+ */
+
 public class Item7 {
-    public static void main(String[] args){
-        System.out.println("This is a test.");
-        for(int i = 0; i < 10; i++){
-            System.out.println(i);
+    static boolean isPrime(int n) {
+        if (n < 2) return false;
+        for (int i = 2; i * i <= n; i++)
+            if (n % i == 0) return false;
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter range (5 10): ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        for (int i = a; i <= b - 2; i++) {
+            if (isPrime(i) && isPrime(i + 2))
+                System.out.println("(" + i + ", " + (i + 2) + ")");
         }
     }
 }
