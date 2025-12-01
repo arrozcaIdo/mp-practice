aeccc78018b0d425ae9390114e58a03943e13aba
Elyssa Marie Manango <mariemanango07@gmail.com>
2025-12-01 20:43:15 +0800

    Automorphic Number

--- machine-problem-2/Item8.java
+++ machine-problem-2/Item8.java
@@ -6,10 +6,16 @@ square ends with the number itself (e.g., 76² = 5776).
  */
 
 public class Item8 {
-    public static void main(String[] args){
-        System.out.println("This is a test.");
-        for(int i = 0; i < 10; i++){
-            System.out.println(i);
-        }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        long sq = (long) n * n;
+        String s = Long.toString(sq);
+        String nStr = Integer.toString(n);
+
+        if (s.endsWith(nStr)) System.out.println("Automorphic Number");
+        else System.out.println("Not an Automorphic Number");
     }
 }
