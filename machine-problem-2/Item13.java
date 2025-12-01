18371dc900dbe886be7826a758e8fd9080d0cbcc
Elyssa Marie Manango <mariemanango07@gmail.com>
2025-12-01 20:46:07 +0800

    Matrix Multiplication

--- machine-problem-2/Item13.java
+++ machine-problem-2/Item13.java
@@ -6,10 +6,42 @@ Display the resulting matrix.
  */
 
 public class Item13 {
-    public static void main(String[] args){
-        System.out.println("This is a test.");
-        for(int i = 0; i < 10; i++){
-            System.out.println(i);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter rows and columns of A: ");
+        int r1 = sc.nextInt(), c1 = sc.nextInt();
+        System.out.print("Enter rows and columns of B: ");
+        int r2 = sc.nextInt(), c2 = sc.nextInt();
+
+        if (c1 != r2) {
+            System.out.println("Multiplication not possible.");
+            return;
+        }
+
+        int[][] A = new int[r1][c1];
+        int[][] B = new int[r2][c2];
+        int[][] C = new int[r1][c2];
+
+        System.out.println("Enter Matrix A:");
+        for (int i = 0; i < r1; i++)
+            for (int j = 0; j < c1; j++)
+                A[i][j] = sc.nextInt();
+
+        System.out.println("Enter Matrix B:");
+        for (int i = 0; i < r2; i++)
+            for (int j = 0; j < c2; j++)
+                B[i][j] = sc.nextInt();
+
+        for (int i = 0; i < r1; i++)
+            for (int j = 0; j < c2; j++)
+                for (int k = 0; k < c1; k++)
+                    C[i][j] += A[i][k] * B[k][j];
+
+        System.out.println("Result:");
+        for (int[] row : C) {
+            for (int x : row) System.out.print(x + " ");
+            System.out.println();
         }
     }
 }
