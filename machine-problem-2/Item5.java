83ae4f60c4ab83573f5900e78ff3b7ce1235a674
Elyssa Marie Manango <mariemanango07@gmail.com>
2025-12-01 19:39:33 +0800

    tama ba to

--- machine-problem-2/Item5.java
+++ machine-problem-2/Item5.java
@@ -8,17 +8,11 @@ public class Item5 {
 
         long sq = (long) n * n;
         String s = Long.toString(sq);
-
         for (int i = 1; i < s.length(); i++) {
             long left = Long.parseLong(s.substring(0, i));
             long right = Long.parseLong(s.substring(i));
 
-            if (right != 0 && left + right == n) {
-                System.out.println("Kaprekar Number");
-                return;
-            }
+            System.out.println(left + right == n ? "Kaprekar Number!" : "NOT a Kaprekar number.");
         }
-
-        System.out.println("Not a Kaprekar Number");
     }
 }
