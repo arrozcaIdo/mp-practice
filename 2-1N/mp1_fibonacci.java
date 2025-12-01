a1819fc2e485860ce1ae7731de3739ba581ca7f1
Elyssa Marie Manango <mariemanango07@gmail.com>
2025-12-01 21:06:29 +0800

    Fibonacci

--- 2-1N/mp1_fibonacci.java
+++ 2-1N/mp1_fibonacci.java
@@ -1,5 +1,31 @@
 public class mp1_fibonacci {
-    public static void main (String[] args){
-        System.out.print("test");
+    public static void main(String[] args){
+        /*
+        • Hardcode a limit n.
+        • Print the first n Fibonacci numbers using a loop.
+        • Challenge: Print the sum of the series.
+        */
+
+        int n = 6;
+        int first = 0;
+        int second = 1;
+        int sum = 0;
+
+        System.out.println("Fibonacci Series for " + n + " times:");
+
+        for (int i = 1; i <= n; i ++){
+            System.out.print(first + " ");
+            sum += first;
+
+            //next number
+            int next = first + second;
+            first = second;
+            second = next;
+        }
+
+        System.out.println("\nSum of the sequence: " + sum);
+
+
+        System.exit(0);
     }
 }
