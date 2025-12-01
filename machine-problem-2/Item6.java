a1ca21027594d7b70f9d8427a6cbe113e069f0f2
Elyssa Marie Manango <mariemanango07@gmail.com>
2025-12-01 20:28:08 +0800

    Prime Factors of a Number

--- machine-problem-2/Item6.java
+++ machine-problem-2/Item6.java
@@ -8,8 +8,17 @@ For example, the prime factors of 84 are 2, 2, 3, 7.
 public class Item6 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
         int n = sc.nextInt();
 
+        System.out.print("Prime Factors: ");
+        for (int i = 2; i * i <= n; i++) {
+            while (n % i == 0) {
+                System.out.print(i + " ");
+                n /= i;
+            }
+        }
+        if (n > 1) System.out.print(n);
 
     }
 }
