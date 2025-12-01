07a8b761b00f7ebd60b94350dd6521b4c2b85907
Elyssa Marie Manango <mariemanango07@gmail.com>
2025-12-01 20:44:51 +0800

    Vowel and Consonant Checker

--- machine-problem-2/Item11.java
+++ machine-problem-2/Item11.java
@@ -5,10 +5,21 @@ Create a program that accepts a string and counts how many vowels and consonants
  */
 
 public class Item11 {
-    public static void main(String[] args){
-        System.out.println("This is a test.");
-        for(int i = 0; i < 10; i++){
-            System.out.println(i);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String s = sc.nextLine().toLowerCase();
+
+        int v = 0, c = 0;
+
+        for (char ch : s.toCharArray()) {
+            if (ch >= 'a' && ch <= 'z') {
+                if ("aeiou".indexOf(ch) != -1) v++;
+                else c++;
+            }
         }
+
+        System.out.println("Vowels: " + v);
+        System.out.println("Consonants: " + c);
     }
 }
