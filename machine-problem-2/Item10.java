c5fa5409676e24647c47598fb77bc87b5c49efab
Elyssa Marie Manango <mariemanango07@gmail.com>
2025-12-01 20:44:17 +0800

    Anagram Checker

--- machine-problem-2/Item10.java
+++ machine-problem-2/Item10.java
@@ -6,10 +6,24 @@ anagrams of each other (contain the same characters in a different order).
  */
 
 public class Item10 {
-    public static void main(String[] args){
-        System.out.println("This is a test.");
-        for(int i = 0; i < 10; i++){
-            System.out.println(i);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first string: ");
+        String a = sc.nextLine();
+        System.out.print("Enter second string: ");
+        String b = sc.nextLine();
+
+        int[] freq = new int[256];
+
+        for (char c : a.toCharArray()) freq[c]++;
+        for (char c : b.toCharArray()) freq[c]--;
+
+        for (int x : freq) {
+            if (x != 0) {
+                System.out.println("Not Anagrams");
+                return;
+            }
         }
+        System.out.println("Anagrams");
     }
 }
