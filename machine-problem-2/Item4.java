63335a198334be7d6aba20b440a8b7dc88b54c46
Elyssa Marie Manango <mariemanango07@gmail.com>
2025-12-01 19:05:41 +0800

    initial

--- machine-problem-2/Item4.java
+++ machine-problem-2/Item4.java
@@ -1,8 +1,26 @@
+import java.util.*;
+
 public class Item4 {
-    public static void main(String[] args){
-        System.out.println("This is a test.");
-        for(int i = 0; i < 10; i++){
-            System.out.println(i);
+        static int sumofSquares(int n){
+            int sum = 0;
+            while (n > 0) {
+                int d = n % 10;
+                sum += d * d;
+                n /= 10;
+            }
+            return sum;
         }
+        public static void main(String[] args){
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter a number: ");
+            int n = sc.nextInt();
+
+            int slow = n, fast = n;
+            do {
+                slow = sumofSquares(slow);
+                fast = sumofSquares(sumofSquares(fast));
+            } while (slow != fast);
+
+            System.out.println(slow == 1 ? "Happy Number!" : "NOT a happy number.");
     }
 }
