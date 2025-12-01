b4ca3e5b0168bdcf810ef1b98475e8ff1edb8a63
Elyssa Marie Manango <mariemanango07@gmail.com>
2025-12-01 20:37:32 +0800

    desc

--- machine-problem-2/Item8.java
+++ machine-problem-2/Item8.java
@@ -1,3 +1,10 @@
+import java.util.*;
+
+/* automorphic-number
+A number is Automorphic if its
+square ends with the number itself (e.g., 76² = 5776).
+ */
+
 public class Item8 {
     public static void main(String[] args){
         System.out.println("This is a test.");
